57d9be3a03ed59ca4efe6d1d04cefc2b10996310
NathanNguyen <khanhngoc0102@gmail.com>
2023-01-24 12:32:32 -0800

    Updates

---
+++ precache-manifest.f724e67223ee3dbfc675be916a61ca73.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a9f200d7a956026ce510685bdea75d9",
+    "url": "/t9/index.html"
+  },
+  {
+    "revision": "6abcc4282a77df7c0f56",
+    "url": "/t9/static/css/main.ad94a53e.chunk.css"
+  },
+  {
+    "revision": "75267617a7cd558b0c24",
+    "url": "/t9/static/js/2.af0d4b87.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/t9/static/js/2.af0d4b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "604a1622df287d1932aa",
+    "url": "/t9/static/js/3.1d36f085.chunk.js"
+  },
+  {
+    "revision": "6abcc4282a77df7c0f56",
+    "url": "/t9/static/js/main.5549bf19.chunk.js"
+  },
+  {
+    "revision": "1d66402068adf9988d2f",
+    "url": "/t9/static/js/runtime-main.acc4a566.js"
+  },
+  {
+    "revision": "e4a20cf406ff0a31378d7ab33ba8c1ba",
+    "url": "/t9/static/media/Owais' Resume.e4a20cf4.pdf"
+  },
+  {
+    "revision": "5035699ad38fd5313bc95c4891f53581",
+    "url": "/t9/static/media/auto-tracker-image.5035699a.png"
+  },
+  {
+    "revision": "09e6ae3da5daad49c278e9ff4a75743d",
+    "url": "/t9/static/media/budget-tracker-image.09e6ae3d.png"
+  },
+  {
+    "revision": "d92efe2500ba8695d4ae6432f75b5cf9",
+    "url": "/t9/static/media/flash-beats-image.d92efe25.gif"
+  },
+  {
+    "revision": "d8671f834c3c32005de3dcb406167c84",
+    "url": "/t9/static/media/living-real-image.d8671f83.png"
+  },
+  {
+    "revision": "fd14fff8beb9ae1bb3fcdf093419fe49",
+    "url": "/t9/static/media/note-taker-image.fd14fff8.png"
+  },
+  {
+    "revision": "0e54790e036d3e97597027712ffaab9b",
+    "url": "/t9/static/media/password-generator-image.0e54790e.png"
+  },
+  {
+    "revision": "e232e25f978fc2e6a3f3960c7dbcb124",
+    "url": "/t9/static/media/recent-photo.e232e25f.jpg"
+  },
+  {
+    "revision": "5db07b5ca4b360c2f9058145e3070ff4",
+    "url": "/t9/static/media/weather-dashboard-image.5db07b5c.png"
+  },
+  {
+    "revision": "2524a8b5b4006da1d062430b9f8b0c05",
+    "url": "/t9/static/media/webpage-screenshot.2524a8b5.png"
+  }
+]);
